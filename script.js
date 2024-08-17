2a2a1729dd13c69eda2a92b4fad974a1183dc032
infob-in <web@infob.in>
2024-08-17 11:55:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+<script>var hideTOC=function(){for(var e=document.getElementsByClassName("toc_list"),t=0;t<e.length;t++)e[t].style.display="none";document.getElementById("showTOC").style.display=""},showTOC=function(){for(var e=document.getElementsByClassName("toc_list"),t=0;t<e.length;t++)e[t].style.display="";document.getElementById("showTOC").style.display="none"}</script>
